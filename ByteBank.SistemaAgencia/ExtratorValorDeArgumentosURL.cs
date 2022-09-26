cbcc672b31c903403369f0ef429a80061ca65738
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2022-09-26 06:59:37 -0300

    Ajustes no construtor do ExtratorValorDeArgumentos e criaçãod o método GetValor.

--- ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
+++ ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -8,21 +8,43 @@ namespace ByteBank.SistemaAgencia
 {
     public class ExtratorValorDeArgumentosURL
     {
+        private readonly string _argumentos;
         public string URL { get; }
 
+        /// <summary>
+        /// Método que verifica se a url é nula ou vazia.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <exception cref="ArgumentException"></exception>
         public ExtratorValorDeArgumentosURL(string url)
         {
-            if (url == null)
-            {
-                throw new ArgumentNullException(nameof(url));
+            if (String.IsNullOrEmpty(url)) {
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio.", nameof(url));
             }
 
-            if (url == "")
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+        }
+
+        /// <summary>
+        /// Método que vai pegar o indexOf do parâmetro.
+        /// </summary>
+        /// <param name="nomeParametro"></param>
+        /// <returns></returns>
+        public string GetValor(string nomeParametro)
+        {
+            string termo = nomeParametro + "=";
+            int indiceTermo = _argumentos.IndexOf(termo);
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if (indiceEComercial == -1)
             {
-                throw new ArgumentException("O argumento url não pode ser uma string vazia.", nameof(url));
+                return resultado; 
             }
 
-            URL = url;
+            return resultado.Remove(indiceEComercial);
         }
     }
 }
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,32 +12,44 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string textoVazio = "";
-            string textoNulo = null;
-            string textoQualquer = "kjhfsdjhgsdfjksdf";
+            //string textoVazio = "";
+            //string textoNulo = null;
+            //string textoQualquer = "kjhfsdjhgsdfjksdf";
 
 
-            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
-            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
-            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+            //Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            //Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+            //Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+            //Console.ReadLine();
+
+
+
+            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+
+            //string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            //int indiceInterrogacao = url.IndexOf('?');
+
+            //Console.WriteLine(indiceInterrogacao);
+
+            //Console.WriteLine(url);
+            //string argumentos = url.Substring(indiceInterrogacao + 1);
+            //Console.WriteLine(argumentos);
+            //Console.ReadLine();
+
+
+
+            string urlParametros = "https://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+
+            string moedaDestino = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino: " + moedaDestino);
+
+            string moedaOrigem = extratorDeValores.GetValor("moedaOrigem");
+            Console.WriteLine("Valor de moedaOrigem: " + moedaOrigem);
+
             Console.ReadLine();
 
-
-
-            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
-
-            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
-
-            int indiceInterrogacao = url.IndexOf('?');
-
-            Console.WriteLine(indiceInterrogacao);
-
-            Console.WriteLine(url);
-            string argumentos = url.Substring(indiceInterrogacao + 1);
-            Console.WriteLine(argumentos);
-
-
-            Console.ReadLine();
         }
     }
 
